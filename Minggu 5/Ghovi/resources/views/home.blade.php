05073c7515301ac2990c0220212a8a19cd465db0
Faiz-Nur-Achmadi <82299706+Faiz-Nur-Achmadi@users.noreply.github.com>
2021-04-13 00:41:34 +0700

    Tugas Minggu 5

---
+++ Minggu 5/Ghovi/resources/views/home.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-', app()->getLocale())}}">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,intial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>{{ config('app.name')}}</title>
+</head>
+<body>
+<div class="jumbotron jumbotron-fluide">
+	<div class="container">
+		<h1 class="display-4">Home Page</h1>
+	<p class="lead">This is the Home Page</p>
+	<p>Nama : {{ "Faiz Nur Achmadi" }}</p>
+<p>Mata Pelajaran</p>
+<ul>
+	<li>Algoritma & Pemrograman</li>
+	<li>Kalkulus</li>
+	<li>Pemrogaman Web</li>
+
+	</div>
+</div>
+</body>
+</html>
---
+++ Minggu 5/Ghovi/resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_','-', app()->getLocale())}}">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width,intial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>{{ config('app.name')}}</title>
+</head>
+<body>
+<div class="jumbotron jumbotron-fluide">
+	<div class="container">
+		<h1 class="display-4">Home Page</h1>
+	<p class="lead">This is the Home Page</p>
+	</div>
+</div>
+</body>
+</html>
