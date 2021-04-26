cad0961f6c380014fbfa1ed99bdd06e744c65f11
Faiz-Nur-Achmadi <82299706+Faiz-Nur-Achmadi@users.noreply.github.com>
2021-04-26 23:09:32 +0700

    Minggu 5

--- Minggu 5/Ghovi/resources/views/image.blade.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html><head>
-<title>Contoh Gambar</title>
-</head><body>
-<img src="https://scontent-cgk1-1.xx.fbcdn.net/v/t1.0-1/cp0/p74x74/95336468_110294234004136_379483415986044928_n.jpg?_nc_cat=105&ccb=1-3&_nc_sid=dbb9e7&_nc_ohc=kLuW-6c-a7AAX_YBWDD&_nc_ht=scontent-cgk1-1.xx&tp=27&oh=e70ce8d4f895755809928ba71e2491e4&oe=60857D99">
-</body></html>
